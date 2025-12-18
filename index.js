96dc870f1966e1758ab20bd615838224544debf9
Mahmoud-Saadaoui <forkan.saadaoui@gmail.com>
2025-12-18 14:25:00 +0100

    feat: Init project with mono

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express')
+const dotenv = require('dotenv')
+dotenv.config()
 
-require('./config/connect')
+require('./src/config/connect')
 
 const app = express()
 app.use(express.json())
 
-app.use('/api/product', require('./routes/productRoute'))
-app.use('/api/user', require('./routes/userRoute'))
+app.use('/api/product', require('./src/modules/products/routes/productRoute'))
+app.use('/api/user', require('./src/modules/users/routes/userRoute'))
 
 app.use('/get_image', express.static('./uploads'))
 
 const PORT = 5000;
-app.listen(PORT, ()=> console.log(`Server Is Running On Port ${PORT}`))
+app.listen(PORT, () => console.log(`Server Is Running On Port ${PORT}`))
--- src/config/connect.js
+++ src/config/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-mongoose.connect('mongodb://127.0.0.1:27017/my_db')
+mongoose.connect(process.env.MONGO_URL)
     .then(
         () => {
             console.log("connected to db")
